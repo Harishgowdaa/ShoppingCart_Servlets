040f795eb22f86c2491e09e94975130c1a07cd61
harish <hgowda515@gmail.com>
2023-03-07 14:53:57 +0530

    added

---
+++ shoppingCart/src/main/java/Controller/AddProduct.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Dao.ProductDao;
+import Dto.Product;
+
+@WebServlet("/addProduct")
+public class AddProduct extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		Product product = new Product();
+		product.setName(req.getParameter("name"));
+		product.setPrice(Double.parseDouble(req.getParameter("price")));
+		product.setCategory(req.getParameter("category"));
+		
+		ProductDao dao= new ProductDao();
+		dao.addProduct(product);
+		
+		resp.getWriter().print("<h1> Product Added Succesfully</h1>");
+		req.getRequestDispatcher("adminHome.html").include(req, resp);
+	}
+
+}
